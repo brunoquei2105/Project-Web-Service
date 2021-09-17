c8fdda5988ce90b667d3028e8569ae5592a65371
brunoquei2105 <bruno.queiroz@liferay.com>
2021-09-17 17:01:36 -0300

    Service layer and component registration

--- src/main/java/com/liferay/course/resources/UserResource.java
+++ src/main/java/com/liferay/course/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.liferay.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.liferay.course.entities.User;
+import com.liferay.course.services.UserService;
+
 
 @RestController
 @RequestMapping(value= "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "bruno", "bruno@gmail.com", "81991819547", "123eja");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> userList = service.findAll();
 		
+		return ResponseEntity.ok().body(userList);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User objUser = service.findById(id);
+		return ResponseEntity.ok().body(objUser);
 	}
 }
