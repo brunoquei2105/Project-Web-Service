7ef4882414fba598125dded092ec2dde0cacf4c7
brunoquei2105 <bruno.queiroz@liferay.com>
2021-09-21 14:45:48 -0300

    SF Products/Category

--- src/main/java/com/liferay/course/entities/Product.java
+++ src/main/java/com/liferay/course/entities/Product.java
@@ -1,12 +1,16 @@
 package com.liferay.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 
 @Entity
@@ -22,12 +26,14 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
+	@Transient
+	private Set<Category> categories = new HashSet<>();
+	
 	public Product() {
 		
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
@@ -73,6 +79,27 @@ public class Product implements Serializable{
 
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
+	}
+	
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
