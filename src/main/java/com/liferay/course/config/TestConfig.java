55522616d5a29ab34d30314bea6a097120b69b5d
brunoquei2105 <bruno.queiroz@liferay.com>
2021-09-22 20:39:51 -0300

    Add relationship many-to-many between Product and Category

--- src/main/java/com/liferay/course/config/TestConfig.java
+++ src/main/java/com/liferay/course/config/TestConfig.java
@@ -20,48 +20,55 @@ import com.liferay.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
-	
+public class TestConfig implements CommandLineRunner {
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Category c1 = new Category(null, "Eletronics");
-		Category c2 = new Category(null,"Books" );
-		Category c3 = new Category(null,"Computers" );
-		
+		Category c2 = new Category(null, "Books");
+		Category c3 = new Category(null, "Computers");
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
-		
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
 		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
-		User u1 = new User(1L, "Mary Brown", "mary@gmail.com","8171147576", "123456");
-		User u2 = new User(2L, "Bob Taylo", "bob@gmail.com","8171146444", "123456");
-		
+
+		p1.getCategories().add(c2);
+		p2.getCategories().add(c1);
+		p2.getCategories().add(c3);
+		p3.getCategories().add(c3);
+		p4.getCategories().add(c3);
+		p5.getCategories().add(c2);
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p4));
+
+		User u1 = new User(1L, "Mary Brown", "mary@gmail.com", "8171147576", "123456");
+		User u2 = new User(2L, "Bob Taylo", "bob@gmail.com", "8171146444", "123456");
+
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
-		
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-	
-		
+
 	}
-	
 
 }
