657b0244f628ad8903f058c33cbe31e7599998a9
brunoquei2105 <bruno.queiroz@liferay.com>
2021-09-26 10:00:38 -0300

    Payment Entity and assciation one-to-one with Order

--- src/main/java/com/liferay/course/config/TestConfig.java
+++ src/main/java/com/liferay/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import java.time.Instant;
 import com.liferay.course.entities.Category;
 import com.liferay.course.entities.Order;
 import com.liferay.course.entities.OrderItem;
+import com.liferay.course.entities.Payment;
 import com.liferay.course.entities.Product;
 import com.liferay.course.entities.User;
 import com.liferay.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment payment1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(payment1);
+		orderRepository.save(o1);
+		
 
 	}
 
