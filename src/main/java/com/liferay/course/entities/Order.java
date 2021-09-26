dcd84d57e1103a7fd0de6c3da366395b4fcd70f8
brunoquei2105 <bruno.queiroz@liferay.com>
2021-09-26 10:15:30 -0300

    Subtotal and Total method

--- src/main/java/com/liferay/course/entities/Order.java
+++ src/main/java/com/liferay/course/entities/Order.java
@@ -105,6 +105,14 @@ public class Order implements Serializable{
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double total = 0;
+		for(OrderItem item : items) {
+			total += item.getSubTotal();
+		}
+		return total;
+	}
 
 	@Override
 	public int hashCode() {
